c391469bfb1eafacc74270ccca07e5c5f4cffaf4
khuongnh <khuongmy1@gmail.com>
2024-12-03 21:39:17 +0700

    add router v2

--- config/web/_routes.php
+++ config/web/_routes.php
@@ -3,9 +3,10 @@ declare(strict_types=1);
 
 return [
     'rules' => [
-        // url => controller
+        // url => controllers
         'GET version' => 'site/version',
         'POST test-request' => 'site/test-request',
-        'POST api/v1/read-excel' => 'api/v1/read-excel/create'
+        'POST api/v1/read-excel' => 'api/v1/read-excel/create',
+        'POST api/v2/read-excel' => 'api/v2/read-excel/create'
     ]
 ];
