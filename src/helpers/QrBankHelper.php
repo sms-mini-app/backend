0825337229ea7e6044e537fb9533d4e5610e2cc9
khuongnh <khuongnh@dtsmart.vn>
2025-04-03 23:40:30 +0700

    fixed amount calculate length

--- src/helpers/QrBankHelper.php
+++ src/helpers/QrBankHelper.php
@@ -12,10 +12,10 @@ class QrBankHelper extends BaseObject
 
     public function generate(): string
     {
-        $template = "00020101021238520010A000000727012200069704160108{{stk}}0208QRIBFTTA53037045405{{amount}}5802VN{{additional_data}}6304";
+        $template = "00020101021238520010A000000727012200069704160108{{stk}}0208QRIBFTTA5303704{{amount}}5802VN{{additional_data}}6304";
         $content = str_replace(["{{stk}}", "{{amount}}", "{{additional_data}}"], [
             $this->accountNoName,
-            $this->amount,
+            $this->getAmount($this->amount),
             $this->getAdditionalDataByNote($this->note)
         ], $template);
         $crc = dechex(CrcHelper::crc16_ibm_3740($content));
@@ -31,5 +31,17 @@ class QrBankHelper extends BaseObject
         $idAdditionalData = "62";
         $lengthAdditionalData = str_pad(strlen($dataNote), 2, "0", STR_PAD_LEFT);
         return $idAdditionalData . $lengthAdditionalData . $dataNote;
+    }
+
+    /**
+     * @param $amount
+     * @return string
+     */
+    public function getAmount($amount): string
+    {
+        $idAmount = "54";
+        $lengthAmount = str_pad(strlen($amount), 2, "0", STR_PAD_LEFT);
+
+        return $idAmount . $lengthAmount . $amount;
     }
 }
