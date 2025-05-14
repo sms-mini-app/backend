64dd315952d5ef389d48a7f2b7b1fb89f36793fa
Khuong Nguyen <khuongdev2001@gmail.com>
2025-05-14 20:25:30 +0700

    add log read file

--- src/modules/v2/controllers/ReadExcelController.php
+++ src/modules/v2/controllers/ReadExcelController.php
@@ -5,6 +5,7 @@ namespace app\modules\v2\controllers;
 use app\components\http\ApiConstant;
 use app\helpers\ArrayHelper;
 use app\helpers\StringHelper;
+use Exception;
 use PhpOffice\PhpSpreadsheet\IOFactory;
 use Yii;
 use yii\web\UploadedFile;
@@ -14,67 +15,79 @@ class ReadExcelController extends Controller
 {
     /**
      * @return array
+     * @throws Exception
      */
     public function actionCreate()
     {
         $file = UploadedFile::getInstanceByName("file");
-        $sheetIndex = Yii::$app->request->post("sheet-index");
-        $sheetIndex = intval($sheetIndex);
-        if (empty($file)) {
-            return $this->responseBuilder->json(false, [], "File not found", ApiConstant::STATUS_BAD_REQUEST);
-        }
-        $reason = [];
-        /** Load $inputFileName to a Spreadsheet Object **/
-        $spreadsheet = IOFactory::load($file->tempName);
-        if ($sheetIndex < 0 || $sheetIndex >= $spreadsheet->getSheetCount()) {
-            $worksheet = $spreadsheet->getActiveSheet();
-        } else {
-            $worksheet = $spreadsheet->getSheet($sheetIndex);
-        }
-        $result = [];
-        $time = time();
-        $ordinalNumbers = 0;
-        $worksheet->toArray();
-        foreach ($worksheet->toArray(null, true, false) as $index => $row) {
-            if ($index == 0) {
-                continue;
+        try {
+            throw new Exception("demo");
+            $sheetIndex = Yii::$app->request->post("sheet-index");
+            $sheetIndex = intval($sheetIndex);
+            if (empty($file)) {
+                return $this->responseBuilder->json(false, [], "File not found", ApiConstant::STATUS_BAD_REQUEST);
             }
-            if (!isset($row[0], $row[2])) {
-                continue;
+            $reason = [];
+            /** Load $inputFileName to a Spreadsheet Object **/
+            $spreadsheet = IOFactory::load($file->tempName);
+            if ($sheetIndex < 0 || $sheetIndex >= $spreadsheet->getSheetCount()) {
+                $worksheet = $spreadsheet->getActiveSheet();
+            } else {
+                $worksheet = $spreadsheet->getSheet($sheetIndex);
             }
-            $column = $index + 1;
-            $fullName = $row[0];
-            $phones = preg_split("/[., ;|]/", $row[2], -1, PREG_SPLIT_NO_EMPTY);
-            foreach ($phones as $phone) {
-                if (preg_match("/^\d{9,20}$/", $phone)) {
-                    $ordinalNumbers++;
-                    $columnReplaces = $this->getColumnsReplace($row, ["c" => $phone]);
-                    $result[] = [
-                        "id" => Uuid::uuid4(),
-                        "fullname" => $this->handleText($fullName),
-                        "phone" => StringHelper::filterPhone($this->handleText($phone)),
-                        "address" => $row[3] ?? "",
-                        "" => $row[3] ?? "",
-                        "option_1" => $row[4] ?? "",
-                        "option_2" => $row[5] ?? "",
-                        "option_3" => $row[6] ?? "",
-                        "column_replaces" => $columnReplaces,
-                        "ordinal_numbers" => $ordinalNumbers
-                    ];
-                } else {
-                    $reason[] = "Ô C$column không hợp lệ";
-                    break;
+            $result = [];
+            $time = time();
+            $ordinalNumbers = 0;
+            $worksheet->toArray();
+            foreach ($worksheet->toArray(null, true, false) as $index => $row) {
+                if ($index == 0) {
+                    continue;
+                }
+                if (!isset($row[0], $row[2])) {
+                    continue;
+                }
+                $column = $index + 1;
+                $fullName = $row[0];
+                $phones = preg_split("/[., ;|]/", $row[2], -1, PREG_SPLIT_NO_EMPTY);
+                foreach ($phones as $phone) {
+                    if (preg_match("/^\d{9,20}$/", $phone)) {
+                        $ordinalNumbers++;
+                        $columnReplaces = $this->getColumnsReplace($row, ["c" => $phone]);
+                        $result[] = [
+                            "id" => Uuid::uuid4(),
+                            "fullname" => $this->handleText($fullName),
+                            "phone" => StringHelper::filterPhone($this->handleText($phone)),
+                            "address" => $row[3] ?? "",
+                            "" => $row[3] ?? "",
+                            "option_1" => $row[4] ?? "",
+                            "option_2" => $row[5] ?? "",
+                            "option_3" => $row[6] ?? "",
+                            "column_replaces" => $columnReplaces,
+                            "ordinal_numbers" => $ordinalNumbers
+                        ];
+                    } else {
+                        $reason[] = "Ô C$column không hợp lệ";
+                        break;
+                    }
                 }
             }
+            if (!empty($_POST["remove_duplicate"])) {
+                $result = ArrayHelper::uniqueColumn($result, "phone");
+            }
+            return $this->responseBuilder->json(true, $result, "Success", ApiConstant::STATUS_OK, [
+                "has_error" => !empty($reason),
+                "reason" => join(PHP_EOL, $reason),
+                "document" => "Đảm bảo cột C là kiểu Text, có thể sử dụng: ;|,. để phân tách nhiều SĐT",
+            ]);
+        } catch (Exception $exception) {
+            $folderPath = Yii::getAlias("@webroot/excel");
+            if (is_dir($folderPath) === false) {
+                mkdir($folderPath, 0777, true);
+            }
+            $filePath = $folderPath . "/" . $file->getBaseName() . "_" . time() . ".xlsx";
+            $file->saveAs($filePath);
+            throw $exception;
         }
-        if (!empty($_POST["remove_duplicate"])) {
-            $result = ArrayHelper::uniqueColumn($result, "phone");
-        }
-        return $this->responseBuilder->json(true, $result, "Success", ApiConstant::STATUS_OK, [
-            "has_error" => !empty($reason),
-            "reason" => join(PHP_EOL, $reason),
-            "document" => "Đảm bảo cột C là kiểu Text, có thể sử dụng: ;|,. để phân tách nhiều SĐT",
-        ]);
     }
 
     /**
