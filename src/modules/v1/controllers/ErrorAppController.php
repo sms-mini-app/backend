c7ac2a307a545373945df148e89d54e56cd7d025
Khuong Nguyen <khuongdev2001@gmail.com>
2025-06-21 23:30:20 +0700

    Add module error app backend

---
+++ src/modules/v1/controllers/ErrorAppController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace app\modules\v1\controllers;
+
+use app\controllers\Controller;
+use Yii;
+
+class ErrorAppController extends Controller
+{
+    public function actionContact()
+    {
+        $this->redirect("https://zalo.me/0394182551");
+    }
+
+
+    public function actionLog()
+    {
+        $error = Yii::$app->request->post("error");
+        if ($error) {
+            $folder = Yii::getAlias("@webroot/logs/errors");
+            if (is_dir($folder) === false) {
+                mkdir($folder, 0777, true);
+            }
+            $filename = "$folder/" . date("Y_m_h") . ".log";
+            $message = date("Y-m-d H:i:s") . " {$error}";
+            file_put_contents($filename, $message . "\n", FILE_APPEND);
+            return "stored";
+        }
+        return "no stored";
+    }
+}
